a5ffd46ab6bca48c4c888164dd0d5bf666c4f29c
Dario Pendic <noblanqme@gmail.com>
2020-08-26 21:18:40 +0200

    added asm_math library

---
+++ asm_lib/Source.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+#include <iostream>
+#include "asm_math.hpp"
+
+int main()
+{	
+	getchar();
+	return 0;
+}
---
+++ asm_lib/asm_math.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace asm_math
+{
+	float asm_abs(float x);
+
+	float asm_mod(float x, float y);
+
+	float asm_floor(float x);
+
+	float asm_sqrt(float x);
+
+	float asm_pow(float x, int y);
+
+	float asm_sin(float x);
+
+	float asm_cos(float x);
+
+	float asm_tan(float x);
+
+	float asm_atan(float x);
+}
